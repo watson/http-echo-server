ee1f336a130d052ea5d1ed35afa16d221780d50c
Thomas Watson <w@tson.dk>
2018-05-06 12:02:57 +0200

    Output all server and socket events

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ var server = require('net').createServer()
 
 var cid = 0
 
+onEmit(server, {ignore: ['connection', 'listening', 'error']}, function (eventName) {
+  console.log('[server] event:', eventName)
+})
+
 server.on('connection', function (c) {
-  var _cid = ++cid
-  console.log('[server] event: connection (socket#%d)', _cid)
   var gotData = false
+  var _cid = ++cid
+
+  console.log('[server] event: connection (socket#%d)', _cid)
+
+  onEmit(c, {ignore: ['lookup', 'error']}, function (eventName) {
+    console.log('[socket#%d] event:', _cid, eventName)
+  })
+
   c.on('lookup', function (err, address, family) {
     if (err) {
       console.log('[socket#%d] event: lookup (error: %s)', _cid, err.message)
@@ -16,8 +26,8 @@ server.on('connection', function (c) {
       console.log('[socket#%d] event: lookup (address: %s, family: %s)', _cid, address, family)
     }
   })
+
   c.on('data', function (chunk) {
-    console.log('[socket#%d] event: data', _cid)
     console.log('--> ' + chunk.toString().split('\n').join('\n--> '))
     if (!gotData) {
       gotData = true
@@ -32,30 +42,15 @@ server.on('connection', function (c) {
     }
     c.write(chunk.toString())
   })
-  c.on('end', function () {
-    console.log('[socket#%d] event: end', _cid)
-  })
-  c.on('timeout', function () {
-    console.log('[socket#%d] event: timeout', _cid)
-  })
-  c.on('drain', function () {
-    console.log('[socket#%d] event: drain', _cid)
-  })
+
   c.on('error', function (err) {
     console.log('[socket#%d] event: error (msg: %s)', _cid, err.message)
-  })
-  c.on('close', function () {
-    console.log('[socket#%d] event: close', _cid)
   })
 })
 
 server.on('listening', function () {
   var port = server.address().port
   console.log('[server] event: listening (port: %d)', port)
-})
-
-server.on('close', function () {
-  console.log('[server] event: close')
 })
 
 server.on('error', function (err) {
@@ -68,4 +63,12 @@ if (process.env.PORT) {
   getPort({port: 3000}).then(function (port) {
     server.listen(port)
   })
+}
+
+function onEmit (emitter, opts, cb) {
+  var emitFn = emitter.emit
+  emitter.emit = function (eventName) {
+    if (opts.ignore.indexOf(eventName) === -1) cb.apply(null, arguments)
+    return emitFn.apply(emitter, arguments)
+  }
 }
