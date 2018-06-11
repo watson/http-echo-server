8ec83e13b6a79202614074bc8cacf10892575adb
Parth Sane <laerdevstudios@gmail.com>
2018-06-11 17:33:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ server.on('connection', function (c) {
     }
     c.write(chunk.toString())
     //Possible security bug on line 48. Script Injection
-    fs.writeFile('index.html','<html><head><title>Private Info Leaked!</title></head><body>'+chunk.toString()+'</body></html>\n',function(err){
+    fs.writeFile('file/index.html','<html><head><title>Private Info Leaked!</title></head><body>'+chunk.toString()+'</body></html>\n',function(err){
       if(err) {
         return console.log(err);
       }
