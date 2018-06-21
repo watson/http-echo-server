afbf8f0a043c1dc24c1a491d6f3e321ff94e392c
Parth Sane <laerdevstudios@gmail.com>
2018-06-21 00:00:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.post('/', function(req, res) {
 });
 app.get('/', function(req, res) {
   var id = req.params.id;
+  console.log('id: '+id);
   var responseBody;
   memjsClient.get(id, function(err,val) {
       console.log('key: %s,value: %s',id,val);
