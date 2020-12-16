b6ac23f8ae75a8a83a7e468948cb8215d209f5ee
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2020-12-16 11:26:53 +0100

    refactor: rename c to socket in connection event

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ onEmit(server, { ignore: ['connection', 'listening', 'error'] }, function (event
   console.log(`[${new Date().toISOString()}][server] event:`, eventName)
 })
 
-server.on('connection', function (c) {
+server.on('connection', function (socket) {
   let gotData = false
   const _cid = ++cid
 
   console.log(`[${new Date().toISOString()}][server] event: connection (socket#${_cid})`)
 
-  onEmit(c, { ignore: ['lookup', 'error'] }, function (eventName) {
+  onEmit(socket, { ignore: ['lookup', 'error'] }, function (eventName) {
     console.log(`[${new Date().toISOString()}][socket#${_cid}] event:`, eventName)
   })
 
-  c.on('lookup', function (err, address, family) {
+  socket.on('lookup', function (err, address, family) {
     if (err) {
       console.log(`[${new Date().toISOString()}][socket#${_cid}] event: lookup (error: ${err.message})`)
     } else {
@@ -31,24 +31,24 @@ server.on('connection', function (c) {
     }
   })
 
-  c.on('data', function (chunk) {
+  socket.on('data', function (chunk) {
     console.log('--> ' + chunk.toString().split('\n').join('\n--> '))
     if (!gotData) {
       gotData = true
-      c.write('HTTP/1.1 200 OK\r\n')
-      c.write('Date: ' + (new Date()).toString() + '\r\n')
-      c.write('Connection: close\r\n')
-      c.write('Content-Type: text/plain\r\n')
-      c.write('Access-Control-Allow-Origin: *\r\n')
-      c.write('\r\n')
+      socket.write('HTTP/1.1 200 OK\r\n')
+      socket.write('Date: ' + (new Date()).toString() + '\r\n')
+      socket.write('Connection: close\r\n')
+      socket.write('Content-Type: text/plain\r\n')
+      socket.write('Access-Control-Allow-Origin: *\r\n')
+      socket.write('\r\n')
       setTimeout(function () {
-        c.end()
+        socket.end()
       }, timeout)
     }
-    c.write(chunk.toString())
+    socket.write(chunk.toString())
   })
 
-  c.on('error', function (err) {
+  socket.on('error', function (err) {
     console.log(`[${new Date().toISOString()}][socket#${_cid}] event: error (msg: ${err.message})`)
   })
 })
